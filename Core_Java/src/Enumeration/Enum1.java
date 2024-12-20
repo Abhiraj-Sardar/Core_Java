c16af3fd1fbb2d8b13a4b052193e6e39d0b3ebcd
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2024-12-21 02:18:16 +0530

    updated Enumeration

---
+++ Core_Java/src/Enumeration/Enum1.java
@@ -0,0 +1,24 @@
+package Enumeration;
+
+enum Sports{
+	BasketBall, FootBall, Cricket, Tennis, Badminton, Swimming, Cycling //This are public static final by default
+}
+
+public class Enum1 {
+
+	public static void main(String[] args) {
+		
+		Sports sp1 = Sports.BasketBall;
+		System.out.println(sp1);
+		
+		Sports sp2 = Sports.Cricket;
+		System.out.println(sp2);
+		
+		Sports sp[] = Sports.values(); //returns a list of enumeration constant
+		
+		for(Sports s : sp) {
+			System.out.println("Sports Value: "+s);
+		}
+	}
+
+}
