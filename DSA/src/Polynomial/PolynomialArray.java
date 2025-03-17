76f55dc3cbe9fb8e536bf5e7990debfa82479dc8
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2025-03-17 19:39:01 +0530

    updated polynomial

--- DSA/src/Polynomial/PolynomialArray.java
+++ DSA/src/Polynomial/PolynomialArray.java
@@ -8,11 +8,47 @@ class Polynomial{
 		this.coeff=arr;
 	}
 	
-	public void addition(int arr1[],int arr2[]) {
+	public Polynomial substraction(Polynomial p) {
+		int add[] = new int[coeff.length];
+		for(int i=coeff.length-1;i>=0;i--) {
+			add[i]=coeff[i]-p.coeff[i];
+		}
+		
+		return new Polynomial(add);
+	}
+	
+	//function for multiply
+	public Polynomial multiply(Polynomial p) {
+		int m=coeff.length;
+		int n=p.coeff.length;
+		int mul[]=new int[m+n-1];
 		
 		for(int i=coeff.length-1;i>=0;i--) {
-			
+			for(int j=coeff.length-1;j>=0;j--) {
+				mul[i+j]= mul[i+j]+coeff[i]*p.coeff[j];
+			}
 		}
+		
+		return new Polynomial(mul);
+	}
+	
+	public Polynomial addition(Polynomial p) {
+		int add[] = new int[coeff.length];
+		for(int i=coeff.length-1;i>=0;i--) {
+			add[i]=coeff[i]+p.coeff[i];
+		}
+		
+		return new Polynomial(add);
+	}
+	
+	// function for polynomial derivation
+	public Polynomial derivation(Polynomial p) {
+		int dev[] = new int[coeff.length-1];
+		for(int i=coeff.length-1;i>0;i--) {
+			dev[i-1]=coeff[i]*i;
+		}
+		
+		return new Polynomial(dev);
 	}
 	
 	public void display() {
@@ -23,13 +59,16 @@ class Polynomial{
 				continue;
 			}
 			
-			if(i==0) {
+			if(i==0){
 				System.out.print(coeff[i]); //print only coefficient i case of only x^0
+			}else if(i==1){
+				System.out.print(coeff[i]+"x+");
 			}
 			else {
 				System.out.print(coeff[i]+"x^"+i+"+");
 			}
 		}
+		System.out.println();
 		
 	}
 	
@@ -38,10 +77,32 @@ class Polynomial{
 public class PolynomialArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int p1[]={1,0,2,3};
-		Polynomial p = new Polynomial(p1);
-		p.display();
+		
+		int p1[]={1,3,2};
+		int p2[]={1,5,3};
+		Polynomial p = new Polynomial(p1); //creating Polynomial
+		Polynomial q = new Polynomial(p2);
+		p.display(); //displaying Polynomial
+		q.display();
+		Polynomial a=p.addition(q);
+		a.display();
+		Polynomial s=q.substraction(p);
+		s.display();
+		
+		int a1[]= {2,5};
+		int a2[]= {1,3};
+		
+		Polynomial A = new Polynomial(a1);
+		Polynomial B = new Polynomial(a2);
+		A.display();
+		B.display();
+		Polynomial C = A.multiply(B);
+		C.display();
+		
+		Polynomial d = new Polynomial(p2);
+		d.display();
+		Polynomial dev = d.derivation(d);
+		dev.display();
 	}
 
 }
